ed7bce66658577f40fe0ede07b49d72ff11af869
kml160030 <kalyb369@hotmail.com>
2021-03-25 11:34:47 -0500

    added a service for registration

---
+++ src/app/registration.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  registerURL = '/register';
+  constructor(private http: HttpClient) { }
+
+  // post method
+  registerAccount(user:User): Observable<User>{
+    return this.http.post<User>(this.registerURL,user);
+  }
+
+}
