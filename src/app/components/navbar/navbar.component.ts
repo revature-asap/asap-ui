662fe91ca8d2811c53404edcceb218e06ba4fd66
Daniel Skwarcha <daniel.skwarcha@revature.net>
2021-03-25 18:11:15 -0400

    Added dynamic changing Navbar with ngFor

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
+  currentUser = "anything"
+
+  publicRoutes = [
+
+    {
+      routeName: 'Dashboard',
+      routeLink: '/dashboard',
+      routeSymbol: 'home'
+    },
+
+    {
+      routeName: 'Login',
+      routeLink: '/login',
+      routeSymbol: 'login'
+    },
+
+  ];
+
+  privateRoutes = [
+    {
+      routeName: 'Dashboard',
+      routeLink: '/dashboard',
+      routeSymbol: 'home'
+    },
+    {
+      routeName: 'User Profile',
+      routeLink: '/profile',
+      routeSymbol: 'person'
+    },
+    {
+      routeName: 'Logout',
+      routeLink: '/dashboard',
+      routeSymbol: 'logout'
+    }
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.currentUser = "Anything";
+  }
+
+  logout() {
+    this.currentUser = '';
   }
 
 }
