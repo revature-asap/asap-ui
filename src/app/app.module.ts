e02d7c5dda8566ae0e88fece0c3ac065ba9d0db4
Eric Newman <googlnewman@gmail.com>
2021-03-26 10:04:51 -0500

    moved imports around in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { AssetDisplayComponent } from './components/asset-display/asset-display.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
