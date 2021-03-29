d1dc110a0087f2fcf53fec159db72a52afbf9b63
Cole Space <cospace@unm.edu>
2021-03-29 09:29:15 -0600

    added the http testing module to the testing files in order to have all the tests pass with no errors

--- src/app/services/finnhub.service.spec.ts
+++ src/app/services/finnhub.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FinnhubService } from './finnhub.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('FinnhubService', () => {
   let service: FinnhubService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(FinnhubService);
   });
 
