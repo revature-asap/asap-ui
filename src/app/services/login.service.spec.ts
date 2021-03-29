7b92eb0b3c8e0f2c743bb89ce97da99db52e6263
Cole Space <cospace@unm.edu>
2021-03-29 09:52:06 -0600

    added more tests to the service for responses from the server

--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -2,7 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Credentials} from "../models/credentials";
+import {Principal} from "../models/principal";
 
 describe('LoginService', () => {
   let service: LoginService;
@@ -34,9 +36,34 @@ describe('LoginService', () => {
     expect(service.grabToken).toEqual('this is a token');
   });
 
-  // it('should login user with correct credentials', () => {
-  //   const username = 'cspace';
-  //   const password = 'password';
-  //   service.authenticate(username, password);
-  // });
+  it('should login user with correct credentials', () => {
+    const creds = new Credentials('cspace', 'password');
+    const principal = new Principal(1, 'cspace', 'basic', 'token');
+
+    const username = 'cspace';
+    const password = 'password';
+    service.authenticate(username, password);
+
+    const req = httpTestingController.expectOne(service.loginUrl);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(creds);
+
+    const expectedResponse = new HttpResponse({ status: 201, statusText: 'Created', body: principal });
+    req.event(expectedResponse);
+  });
+
+  it('should return a 404 if the endpoint is not found', () => {
+
+    const username = 'blahblah';
+    const password = 'halbhalb';
+    service.authenticate(username, password);
+
+    const req = httpTestingController.expectOne(service.loginUrl);
+
+    const msg = '404';
+    req.flush(msg, {
+      status: 404,
+      statusText: 'Not Found'
+    });
+  });
 });
