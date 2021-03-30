1ee7f0bb7b597dfd3a25e6bf04b4271eb3165a1d
Cole Space <cospace@unm.edu>
2021-03-30 13:56:44 -0600

    changed from the newscatcher api to the finnhub api and was able to display the article information, as well as have a link that redirects to the article url

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NewsService} from "../../services/news.service";
+import {Article} from "../../models/article";
 
 @Component({
   selector: 'app-news',
@@ -8,17 +9,34 @@ import {NewsService} from "../../services/news.service";
 })
 export class NewsComponent implements OnInit {
 
-  articles: any[] = [];
+  articles: Article[] = [];
 
   constructor(private newsService: NewsService) { }
 
   fetchArticles = async () => {
+    //Finnhub is annoying
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    let to = yyyy + '-' + mm + '-' + dd;
+
+    let weekAgo = new Date(today.getTime() - (7 * 24 * 60 * 60 * 1000));
+    let wad = String(weekAgo.getDate()).padStart(2, '0');
+    let wam = String(weekAgo.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let way = weekAgo.getFullYear();
+    let from = way + '-' + wam + '-' + wad;
 
     try{
-      this.articles = await this.newsService.stockNews();
+      this.articles = await this.newsService.stockNews('AAPL', from, to);
+      console.log(this.articles);
     }catch (e){
       console.error(e);
     }
+  }
+
+  redirect(url: string){
+    window.open(url, '_blank');
   }
 
   ngOnInit(): void {
