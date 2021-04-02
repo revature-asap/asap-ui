282ad7c9e6607e459d80e79d0e45e4b91521b057
Daniel Skwarcha <daniel.skwarcha@revature.net>
2021-04-02 14:26:33 -0400

    Edited Principal class

--- src/app/models/principal.ts
+++ src/app/models/principal.ts
@@ -1,12 +1,18 @@
 export class Principal {
   id: number;
   username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
   role: string;
   token: string;
 
-  constructor(id: number, username: string, role: string, token: string) {
+  constructor(id: number, username: string, firstName: string, lastName: string, email: string,  role: string, token: string) {
     this.id = id;
     this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
     this.role = role;
     this.token = token;
   }
