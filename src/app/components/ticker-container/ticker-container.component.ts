f27b1f596a7e64429004580d72a6ed56e473a171
Cole Space <cospace@unm.edu>
2021-04-06 12:38:44 -0600

    trying to list some common tickers

--- src/app/components/ticker-container/ticker-container.component.ts
+++ src/app/components/ticker-container/ticker-container.component.ts
@@ -3,6 +3,8 @@ import { FinnhubService } from '../../services/finnhub.service';
 import { assetQuote } from '../../models/assetQuote.model'
 import { companyProfile } from 'src/app/models/companyProfile';
 import { TickerService } from 'src/app/services/ticker.service';
+import {LoginService} from "../../services/login.service";
+import {WatchListService} from "../../services/watch-list.service";
 
 @Component({
   selector: 'app-ticker-container',
@@ -11,27 +13,46 @@ import { TickerService } from 'src/app/services/ticker.service';
 })
 export class TickerContainerComponent implements OnInit {
 
+  stringOfAssets: string[] = [];
   asset!: assetQuote;
   change!: number;
   assetQuotes:assetQuote[] = []; // = [new assetQuote(), new assetQuote(), new assetQuote(), new assetQuote(), new assetQuote()];
-  constructor(private finnhubService:FinnhubService, public tickerService: TickerService) { }
+  constructor(private finnhubService:FinnhubService, public tickerService: TickerService, private loginService: LoginService, private watchlistService: WatchListService) { }
 
   ngOnInit(): void {
+    this.assetQuotes = [];
+    let assetNames: string[] = [];
+    this.loginService.currentUser$.subscribe(
+      async (user) => {
+        if (user != null) {
+          let companies = await this.watchlistService.fetchUserWatchList();
+          if(companies.length > 0){
+            for (const company of companies) {
+              assetNames.push(company.ticker);
+            }
+          }
+        }else{
+          assetNames = ['AAPL', 'GME', 'GOOG', 'AMZN', 'MSFT', 'TSLA'];
+        }
+      });
 
-    this.finnhubService.getQuote("IBM")
+    for (const assetName of assetNames) {
+      console.log(assetName);
+      this.finnhubService.getQuote(assetName)
         .subscribe((asseta)=>{
           this.asset = new assetQuote(asseta);
-          this.asset.companyTicker = "IBM";
+          this.asset.companyTicker = assetName;
 
           this.change = this.tickerService.computeChange(this.asset.current, this.asset.previousClose);
 
-          this.finnhubService.getProfile("IBM")
-          .subscribe((profile: companyProfile) => {
-            this.asset.companyName = profile.name;
-            this.assetQuotes.push(this.asset);
-          });
+          this.finnhubService.getProfile(assetName)
+            .subscribe((profile: companyProfile) => {
+              this.asset.companyName = profile.name;
+              this.assetQuotes.push(this.asset);
+            });
 
         });
+    }
   }
 
 }
