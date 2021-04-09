afa8738eca1c882834db4baa5ce88943c5050572
kml160030 <kalyb369@hotmail.com>
2021-04-09 09:31:21 -0500

    added documentation for registration component and registration service

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -7,12 +7,19 @@ import { User } from '../models/user';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * registration service that is just for adding a post method to our register endpoint on the backend
+ */
 export class RegistrationService {
   registerURL = 'http://ec2co-ecsel-1g0q6xc63i5af-1652680293.us-east-2.elb.amazonaws.com:5000/users';
   // registerURL = '/users';
   constructor(private http: HttpClient) { }
 
-  // post method
+  /**
+   *  Post Method to connect to backend
+   * @param user the user that is to be registered by taking the input from fields that someone has filled out
+   * @returns an observable that can be subscribed to of type User
+   */
   register(user:User): Observable<User>{
     console.log("registration service");
 
